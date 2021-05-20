3a75aaa2e9a822e2b20a9f235aec521ba231cdcc
rsneh <me@ronsneh.com>
2021-05-20 18:32:16 +0300

    Change Logo to CC instead of FC

--- src/Components/Layout/Logo/Logo.tsx
+++ src/Components/Layout/Logo/Logo.tsx
@@ -1,9 +1,22 @@
+import { Component } from "react";
 import logoImage from "../../../Assets/Images/northwind-logo.jpg";
 
-export default function Logo(): JSX.Element {
-  return (
-    <div className="logo-container">
-      <img src={logoImage} alt="Northwind Logo" width={250} />
-    </div>
-  )
+// function Logo(): JSX.Element {
+//   return (
+//     <div className="logo-container">
+//       <img src={logoImage} alt="Northwind Logo" width={250} />
+//     </div>
+//   )
+// }
+
+class Logo extends Component {
+  public render(): JSX.Element {
+    return (
+      <div className="logo-container">
+        <img src={logoImage} alt="Northwind Logo" width={250} />
+      </div>
+    )
+  }
 }
+
+export default Logo;
